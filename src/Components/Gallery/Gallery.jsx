403bcdd5476a6051345d72fe7d0e1551824d1e0d
Pujon Das Auvi <auvipujondas@gmail.com>
2023-05-19 23:31:35 +0600

    gallery complete

---
+++ src/Components/Gallery/Gallery.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+import { Gallery } from "react-grid-gallery";
+
+import banner1 from "../../assets/Banner/banner1.jpg";
+import banner2 from "../../assets/Banner/banner2.jpg";
+import banner3 from "../../assets/Banner/banner3.jpg";
+
+const Gallery1 = () => {
+  const images = [
+    {
+      src: `${banner2}`,
+      width: 270,
+      height: 320,
+    },
+    {
+      src: `${banner2}`,
+      width: 340,
+      height: 190,
+    },
+    {
+      src: `${banner2}`,
+      width: 321,
+      height: 148,
+    },
+    {
+      src: `${banner2}`,
+      width: 314,
+      height: 213,
+    },
+    {
+      src: `${banner2}`,
+      width: 320,
+      height: 213,
+    },
+    {
+      src: `${banner2}`,
+      width: 320,
+      height: 190,
+    },
+
+    {
+      src: `${banner2}`,
+      width: 320,
+      height: 113,
+    },
+    {
+      src: `${banner2}`,
+      width: 315,
+      height: 320,
+    },
+    {
+      src: `${banner2}`,
+      width: 305,
+      height: 340,
+    },
+    {
+      src: `${banner2}`,
+      width: 320,
+      height: 190,
+    },
+    {
+      src: `${banner2}`,
+      width: 320,
+      height: 148,
+    },
+    {
+      src: `${banner2}`,
+      width: 320,
+      height: 213,
+    },
+    {
+      src: `${banner2}`,
+      width: 320,
+      height: 213,
+    },
+    {
+      src: `${banner2}`,
+      width: 248,
+      height: 320,
+    },
+    {
+      src: `${banner2}`,
+      width: 230,
+      height: 113,
+    },
+  ];
+  return (
+    <div className="h-96 overflow-y-scroll shadow-2xl flex-1">
+      <Gallery images={images} enableImageSelection={false} />
+    </div>
+  );
+};
+
+export default Gallery1;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 
+import Gallery1 from "../../Components/Gallery/Gallery";
+
 const Home = () => {
   return (
     <div>
       <Header />
+      <div className="flex justify-center items-center gap-3 sm:flex-col md:flex-row mx-3 h-screen ">
+        <div className="text-6xl">Our Toy Gallery</div>
+        <Gallery1 />
+      </div>
     </div>
   );
 };
